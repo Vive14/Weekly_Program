6010575d0d7e324fe74e7efedb74a7296a096d19
Game <86363360+Vive14@users.noreply.github.com>
2021-09-02 21:29:29 +0700

    Create Week _Program4.1.c

---
+++ Week _Program4.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter Number -> ");
+    scanf("%d", &n);
+    for (int x = 1; x <= n; x++)
+    {
+        for (int y = 1; y <= n; y++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
